2bea5c63491a7bf87e5e798da6c01d70984e2f86
ALEXkaratepacan <alexkaratepacan@gmail.com>
2024-11-21 10:21:03 +0300

    Second commit

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+number = np.random.randint(1, 100)  # загадываем число
+count = 0  # создаём переменную счётчик для учёта количества попыток
+
+while True:
+    count += 1
+    try:
+        predict_number = int(input("Угадай число от 1 до 100: "))
+        
+        if predict_number > number:
+            print("Число должно быть меньше!")
+        
+        elif predict_number < number:
+            print("Число должно быть больше!")
+        
+        else:
+            print(f"Вы угадали число! Это число = {number}, за {count} попыток.")
+            break  # конец игры, выход из цикла
+    except ValueError:
+        print("Пожалуйста, введите целое число.")
+        
+        
+        
+        
